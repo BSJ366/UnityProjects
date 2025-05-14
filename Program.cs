4f37b053992922e49d46a51c15247a0f01895be3
BSJ366 <rjdswns366@gmail.com>
2025-05-14 10:13:39 +0900

    비쥬얼스튜디오에서 작성한 C#코드
    switch의 식 표현을 일반적인 switch문으로 재구성

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Cache;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _0513_CSharp
+{
+    internal class Program
+    {
+       private static double GetDiscountRate(object client)
+        {
+            
+
+          switch (client)
+            {
+                case ("학생", int n):
+                    {
+                        if (n < 18)
+                            return 0.2;
+                        else
+                            return 0.1;
+                    }
+                case ("일반", int n):
+                    {
+                        if (n < 18)
+                            return 0.1;
+                        else
+                            return 0.05;
+                    }
+                case (_):
+                    return 0;
+            }
+            
+        }
+        
+        static void Main(string[] args)
+        {
+            var a = ("바보", 2 );
+            Console.WriteLine($"{GetDiscountRate(a)}");
+        }
+    }
+}
